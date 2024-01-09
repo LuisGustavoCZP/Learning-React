bc6348eecbd84b4f3ff67ac513c5b57e4bcda877
Luis Gustavo Zanetti <49000286+LuisGustavoCZP@users.noreply.github.com>
2024-01-09 20:50:06 -0300

    ajusting main without MUI

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,24 +1,27 @@
-import { Box, Typography } from "@mui/material";
+import { Button } from "../components/Button";
 import { LogoIcon } from "../components/LogoIcon";
+import "../styles/main.css";
 
 export function MainPage ({logo})
 {
-    return (
-        <Box>
-            <Box 
-                component="header"
-                sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                }}
-            >
-                <Typography>Calculadora</Typography>
-                <LogoIcon src={logo} alt="logo" />
-            </Box>
-            <Box component="main">
+    const numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].map((numero) => {
+        return (<Button size={50} onClick={() => console.log(numero)}>{numero}</Button>);
+    });
+
+    const operadores = ["+", "-", "*", "/"].map((operador) => {
+        return (<Button size={50} onClick={() => console.log(operador)}>{operador}</Button>);
+    });
     
-            </Box>
-        </Box>
+    return (
+        <div className="calculadora-root">
+            <header>
+                <h1>Calculadora</h1>
+                <LogoIcon src={logo} alt="logo" />
+            </header>
+            <div style={{width: "400px", height: "700px"}}>
+                {numeros}
+                {operadores}
+            </div>
+        </div>
     );
 }
